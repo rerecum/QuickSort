b0640d9e04dc53a5e294ec779bf4b353ab2f78fe
rerecum <kacperkacperkacper132@gmail.com>
2021-12-31 00:07:40 +0100

    Dodanie voida quicksort z funkcjami oraz skopiowanie kodu z sortowania babelkowego i zmiana nazw z babelkowy na "szybko" itp

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,7 +1,79 @@
-﻿
+﻿//QuickSort
+//Kacper Hnatyszyn 3prT5
+
 #include <iostream>
+#include <time.h>
+#include <windows.h>
+
+using namespace std;
+
+int ilosc;
+clock_t start, stop;
+double czas;
+
+void quicksort(int* tablica, int lewy, int prawy)
+{
+	int v = tablica[(lewy + prawy) / 2];
+	int i, j, x;
+	i = lewy;
+	j = prawy;
+	do {
+		while (tablica[i] < v) i++;
+		while (tablica[j] > v) j--;
+		if (i <= j) {
+			x = tablica[i];
+			tablica[i] = tablica[j];
+			tablica[j] = x;
+			i++; j--;
+			}
+	} while (i <= j);
+
+	if (j > lewy) quicksort(tablica, lewy, j);
+	if (i < prawy) quicksort(tablica, i, prawy);
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Porowaneni czasow sortowania v.1" << endl;
+
+	cout << "Ile losowych liczb w tablicy: ";
+	cin >> ilosc;
+
+	//dynamiczna alokacja tablicy
+	int* tablica;
+	tablica = new int[ilosc];
+
+	//inicjowanie generatora
+	srand(time(NULL));
+
+	//wczytywanie  liczb do tablicy
+	for (int i = 0; i < ilosc; i++)
+	{
+		tablica[i] = rand() % 1000000 + 1;
+	}
+
+	cout << "Przed posortowaniem: " << endl;
+	for (int i = 0; i < ilosc; i++)
+	{
+		cout << tablica[i] << " ";
+	}
+
+	cout << endl << "Sortuje teraz szybkoo. Prosze czekac!" << endl;
+
+	start = clock();
+	quicksort(tablica, 0, ilosc-1);
+	stop = clock();
+	czas = (double)(stop - start) / CLOCKS_PER_SEC;
+
+	cout << "Po posortowaniu: " << endl;
+	for (int i = 0; i < ilosc; i++)
+	{
+		cout << tablica[i] << " ";
+	}
+
+	cout << endl << "Czas sortowania szybkiegoooo: " << czas << " s" << endl;
+
+	delete[] tablica;
+
+	return 0;
 }
